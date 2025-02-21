8c2bb3fbd27e3090e55ef4d6e944d14edd5e3c95
rbms <rbms.labanos2024@gmail.com>
2025-02-21 22:20:12 +0800

    updated

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -26,10 +26,13 @@ function MyNavbar() {
         </Navbar.Brand>
 
         <Navbar.Toggle
-          aria-controls="offcanvasNavbar-expand-md"
-          onClick={() => setShowOffcanvas(true)}
-          className="custom-toggle"
-        />
+        aria-controls="offcanvasNavbar-expand-md"
+        onClick={() => setShowOffcanvas(true)}
+        className="custom-toggle"
+      >
+        <span className="mini-label">Menu</span>
+      </Navbar.Toggle>
+
         <Navbar.Offcanvas
           id="offcanvasNavbar-expand-md"
           aria-labelledby="offcanvasNavbarLabel-expand-md"
